df172bc64adde22372b280a0865a68aa4dbd002b
Mandiggo <mandico1990@gmail.com>
2024-04-13 19:13:20 +0300

    Initial commit

---
+++ src/test/java/com/example/sqlzadanie/SqLzadanieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlzadanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqLzadanieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
